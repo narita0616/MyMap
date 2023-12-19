295cd4d62d1ef89722a8d7439593f5b8c7bc892b
DJmatsunaga <naritari0616@icloud.com>
2023-12-20 00:44:10 +0900

    Initial Commit

---
+++ MyMap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyMap
+//
+//  Created by 成田陸人 on 2023/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by 成田陸人 on 2023/12/20.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

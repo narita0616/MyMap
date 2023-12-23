224444d605c1440e092ff68114048b140a9bf3bd
DJmatsunaga <naritari0616@icloud.com>
2023-12-23 11:01:40 +0900

    地図アプリ完成

--- MyMap/MapView.swift
+++ MyMap/MapView.swift
@@ -17,13 +17,26 @@ enum MapType {
 struct MapView: View {
     let searchKey: String
     let mapType: MapType
+    
     @State var targetCoordinate = CLLocationCoordinate2D()
     @State var cameraPosition: MapCameraPosition = .automatic
+    
+    var mapStyle: MapStyle{
+        switch mapType {
+        case .standard:
+            return MapStyle.standard()
+        case .satellite:
+            return MapStyle.imagery()
+        case .hybrid:
+            return MapStyle.hybrid()
+        }
+    }
     
     var body: some View {
         Map(position: $cameraPosition){
             Marker(searchKey, coordinate: targetCoordinate)
         }
+        .mapStyle(mapStyle)
         .onChange(of: searchKey, initial: true) {   oldValue, newValue in
             print("検索キーワード:\(newValue)")
             
@@ -50,5 +63,5 @@ struct MapView: View {
 }
 
 #Preview {
-    MapView(searchKey: "東京駅")
+    MapView(searchKey: "東京駅", mapType: .standard)
 }
